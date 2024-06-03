cc461791f050ab97e1641990efa2f975a9cf8642
Giovani Schiar <giovanischiar@gmail.com>
2024-06-03 16:06:44 -0700

    Isolate Navigation Inside its Own Package

--- app/src/main/java/io/schiar/pokechart/MainActivity.kt
+++ app/src/main/java/io/schiar/pokechart/MainActivity.kt
@@ -5,38 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.wear.compose.navigation.SwipeDismissableNavHost
-import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 import dagger.hilt.android.AndroidEntryPoint
-import io.schiar.pokechart.view.resulttype.resultTypeScreen
-import io.schiar.pokechart.view.shared.Route
-import io.schiar.pokechart.view.shared.Route.TYPES_ROUTE
-import io.schiar.pokechart.view.shared.theme.PokechartTheme
-import io.schiar.pokechart.view.types.typesScreen
+import io.schiar.pokechart.view.navigation.Navigation
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { Navigation() }
-    }
-
-    @Composable
-    fun Navigation(navController: NavHostController = rememberSwipeDismissableNavController()) {
-        PokechartTheme {
-            SwipeDismissableNavHost(
-                navController = navController,
-                startDestination = TYPES_ROUTE.id
-            ) {
-                typesScreen(
-                    navigateToResultTypes = {
-                        navController.navigate(route = Route.RESULT_TYPE_ROUTE.id)
-                    }
-                )
-                resultTypeScreen()
-            }
-        }
     }
 
     @Preview(showBackground = true)
---
+++ app/src/main/java/io/schiar/pokechart/view/navigation/Navigation.kt
@@ -0,0 +1,27 @@
+package io.schiar.pokechart.view.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.wear.compose.navigation.SwipeDismissableNavHost
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import io.schiar.pokechart.view.resulttype.resultTypeScreen
+import io.schiar.pokechart.view.shared.Route
+import io.schiar.pokechart.view.shared.theme.PokechartTheme
+import io.schiar.pokechart.view.types.typesScreen
+
+@Composable
+fun Navigation(navController: NavHostController = rememberSwipeDismissableNavController()) {
+    PokechartTheme {
+        SwipeDismissableNavHost(
+            navController = navController,
+            startDestination = Route.TYPES_ROUTE.id
+        ) {
+            typesScreen(
+                navigateToResultTypes = {
+                    navController.navigate(route = Route.RESULT_TYPE_ROUTE.id)
+                }
+            )
+            resultTypeScreen()
+        }
+    }
+}
